e6c332826e5e14dd6d592b252c5536899df632d5
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-02 17:10:55 +0100

    Final version

--- 2022/Day_1/day1.py
+++ 2022/Day_1/day1.py
@@ -6,13 +6,13 @@ elf_index_max = 0
 with open('Input/Input.txt') as f:
     for line in f:
             if line != '\n':
-                sum += int(line.strip())
+                sum += int(line)
             else:
                 elf_index += 1
                 if sum > max_sum:
                     max_sum = sum
                     elf_index_max = elf_index
-                    sum = 0
+                sum = 0
 
 
 print(max_sum)
