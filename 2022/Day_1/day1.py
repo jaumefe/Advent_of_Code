57e3055f81aee646ae579fb0dc9099ca2792a641
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-03 22:22:51 +0100

    Modified for obtaining the second star

--- 2022/Day_1/day1.py
+++ 2022/Day_1/day1.py
@@ -1,7 +1,7 @@
-max_sum = 0
 sum = 0
 elf_index = 0
 elf_index_max = 0
+max_sums = [0, 0, 0]
 
 with open('Input/Input.txt') as f:
     for line in f:
@@ -9,13 +9,18 @@ with open('Input/Input.txt') as f:
                 sum += int(line)
             else:
                 elf_index += 1
-                if sum > max_sum:
-                    max_sum = sum
+                if sum > max(max_sums):
+                    max_sums[2] = max_sums[1]
+                    max_sums[1] = max_sums[0]
+                    max_sums[0] = sum
                     elf_index_max = elf_index
+                elif sum > min(max_sums):
+                    if sum > max_sums[1]:
+                        max_sums[2] = max_sums[1]
+                        max_sums[1] = sum
+                    elif sum > max_sums[2] and sum < max_sums[1]:
+                        max_sums[2] = 2
                 sum = 0
-
-
-print(max_sum)
-print('\n')
+print(max(max_sums))
+print(max_sums[0] + max_sums[1] + max_sums[2])
 print(elf_index_max)
-print('\n')
