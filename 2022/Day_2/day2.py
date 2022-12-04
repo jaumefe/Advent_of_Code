82c4925c2eba4bfcc334f3a07e68a32824996c44
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-04 13:31:53 +0100

    Second part of Day 2 exercise

--- 2022/Day_2/day2.py
+++ 2022/Day_2/day2.py
@@ -1,4 +1,5 @@
 result = 0
+# Part 1
 with open('Input/Inputs.txt') as f:
     for line in f:
         A = line.split()
@@ -23,6 +24,40 @@ with open('Input/Inputs.txt') as f:
             result += 6
         elif A[1] == 'C' and A[0] == 'B':
             result += 6
+print("First part result: " + str(result))
+
+# Part 2
+result = 0
+with open('Input/Inputs.txt') as f:
+    for line in f:
+        A = line.split()
+        # Need to lose
+        if A[1] == 'X':
+            result += 0
+            if A[0] == 'A':
+                result += 3
+            elif A[0] == 'B':
+                result += 1
+            elif A[0] == 'C':
+                result += 2
+        # Need to draw
+        elif A[1] == 'Y':
+            result += 3
+            if A[0] == 'A':
+                result += 1
+            elif A[0] == 'B':
+                result += 2
+            elif A[0] == 'C':
+                result += 3
+        # Need to win
+        elif A[1] == 'Z':
+            result += 6
+            if A[0] == 'A':
+                result += 2
+            elif A[0] == 'B':
+                result += 3
+            elif A[0] == 'C':
+                result += 1
 
 
-print(result)
+print("Second part result: " + str(result))
