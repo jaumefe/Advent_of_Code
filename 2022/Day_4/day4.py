fcd1a304f7f0d53b929986963a6e2f869fe9b7f0
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-04 14:14:05 +0100

    Day 4 Part 2 uploaded

--- 2022/Day_4/day4.py
+++ 2022/Day_4/day4.py
@@ -1,3 +1,6 @@
+import pandas as pd
+# Part 1
+
 count = 0
 
 with open('Input/Input.txt') as f:
@@ -10,7 +13,22 @@ with open('Input/Input.txt') as f:
         elif (int(A2[0]) <= int(A1[0])) and (int(A2[1]) >= int(A1[1])):
             count += 1
 
+print('First part answer is: ' + str(count))
 
-print(count)
+# Part 2
+
+count = 0
+
+with open('Input/Input.txt') as f:
+    for line in f:
+        A = line.split(',')
+        A1 = A[0].split('-')
+        A2 = A[1].split('-')
+        i1 = pd.Interval(int(A1[0]), int(A1[1]), closed='both')
+        i2 = pd.Interval(int(A2[0]), int(A2[1]), closed='both')
+        if i1.overlaps(i2):
+            count += 1
+
+print('Second part answer is: ' + str(count))
 
 
