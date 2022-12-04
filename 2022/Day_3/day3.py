d795213ff8a2c46aaaa53caa6682b82708d1292b
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-04 14:06:47 +0100

    Second part of Day 3 problem

--- 2022/Day_3/day3.py
+++ 2022/Day_3/day3.py
@@ -1,5 +1,7 @@
 import string
 
+# Part 1
+
 item_type = []
 rep_item = ''
 sum = 0
@@ -22,4 +24,32 @@ for i in range(len(item_type)):
     else:
         sum += string.ascii_lowercase.index(item_type[i]) + 1
 
-print(sum)
+print('First part answer is: ' + str(sum))
+
+# Part 2
+item_type = []
+rep_item = ''
+sum = 0
+n = 0
+
+with open('Input/Input.txt') as f:
+    lines = f.readlines()
+
+while n < len(lines):
+    A = lines[n:(3+n)]
+    for i in range(len(A[0].strip())):
+        for j in range(len(A[1].strip())):
+            for k in range(len(A[2].strip())):
+                if A[0][i] == A[1][j] and A[0][i] == A[2][k]:
+                    rep_item = A[0][i]
+    item_type.append(rep_item)
+    n += 3
+
+for i in range(len(item_type)):
+    if (item_type[i]).isupper():
+        sum += string.ascii_uppercase.index(item_type[i]) + 1 + 26
+    else:
+        sum += string.ascii_lowercase.index(item_type[i]) + 1
+
+print('Second part answer is: ' + str(sum))
+
