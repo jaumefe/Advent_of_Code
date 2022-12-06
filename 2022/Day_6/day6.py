6455e8aab4764c05da827611ad69af2ea51512f6
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-06 11:53:57 +0100

    Second part of day 6 problem

--- 2022/Day_6/day6.py
+++ 2022/Day_6/day6.py
@@ -3,6 +3,8 @@ with open("Input/input.txt") as f:
 
 count = 4
 
+# Part 1
+
 for i in range(len(lines[0].strip())-4):
     rep_item = 0
     marker = lines[0][i:(4+i)]
@@ -18,4 +20,25 @@ for i in range(len(lines[0].strip())-4):
     if not rep_item:
         break
 
-print(count)
+print('First part answer is: ' + str(count))
+
+# Part 2
+
+count = 14
+
+for i in range(len(lines[0].strip())-4):
+    rep_item = 0
+    marker = lines[0][i:(14+i)]
+    for j in range(len(marker)):
+        for k in range(len(marker)):
+            if j < k:
+                if marker[j] == marker[k]:
+                    rep_item = 1
+                    break
+        if rep_item:
+            count += 1
+            break
+    if not rep_item:
+        break
+
+print('Second part answer is: ' + str(count))
