9dde08e11082f40ac4ad57f7ad65d200877e0f2d
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-12-10 16:56:41 +0100

    Part 2 Day 10 Problem

--- 2022/Day_10/day10.py
+++ 2022/Day_10/day10.py
@@ -9,16 +9,43 @@ def signalStrength (cycle, inst):
         if row[0] == 'addx':
             n += 2
             if n >= cycle:
-                return cycle*reg
+                return reg
             reg += int(row[1])
         elif row[0] == 'noop':
             n += 1
             if n >= cycle:
-                return cycle*reg
+                return reg
+
+## Part 1
 
 result = 0
 
 for i in range(20, 220 + 40, 40):
-    result += signalStrength(i, lines)
+    result += i*signalStrength(i, lines)
 
 print(result)
+
+## Part 2
+spriteLen = 3
+CRT = [40, 6]
+screen = []
+
+result = 0
+
+for i in range(CRT[1]):
+    screen.append(['']*CRT[0])
+
+for j in range(0, 6):
+    for i in range(0, 40):
+        result = signalStrength(i+1+40*j, lines)
+        if i == result - 1:
+            screen[j][i] = '#'
+        elif i == result:
+            screen[j][i] = '#'
+        elif i == result + 1:
+            screen[j][i] = '#'
+        else:
+            screen[j][i] = '.'
+
+for i in range(len(screen)):
+    print(screen[i])
