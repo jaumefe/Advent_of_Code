47f9623f0c5ce7e268d20b4f1e5f297660574989
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2023-12-02 16:36:16 +0100

    Day 1 Part 2 added

--- 2023/Day_01/01.go
+++ 2023/Day_01/01.go
@@ -18,7 +18,7 @@ func main() {
 	}
 	pt1 := part1(string(input1))
 	fmt.Println("The Fisrt part result is:", pt1)
-	input2, err := inputFS.ReadFile("inputs/test.txt")
+	input2, err := inputFS.ReadFile("inputs/input.txt")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -47,23 +47,68 @@ func part1(input string) int {
 
 func part2(input string) int {
 	var result int
-	numbers := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 	in := strings.Split(input, "\n")
 	for _, i := range in {
+		var line []int
 		for k, j := range strings.Split(i, "") {
-			switch j {
-			case "o":
-			case "t":
-				if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2] == "two" {
-					fmt.Println(j + strings.Split(i, "")[k+1] + strings.Split(i, "")[k+2])
+			num, err := strconv.Atoi(j)
+			if err == nil {
+				line = append(line, num)
+			} else {
+				switch j {
+				case "o":
+					if k+2 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2] == "one" {
+							line = append(line, 1)
+						}
+					}
+				case "t":
+					if k+2 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2] == "two" {
+							line = append(line, 2)
+						}
+					}
+					if k+4 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3]+strings.Split(i, "")[k+4] == "three" {
+							line = append(line, 3)
+						}
+					}
+				case "f":
+					if k+3 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3] == "four" {
+							line = append(line, 4)
+						}
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3] == "five" {
+							line = append(line, 5)
+						}
+					}
+				case "s":
+					if k+2 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2] == "six" {
+							line = append(line, 6)
+						}
+					}
+					if k+4 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3]+strings.Split(i, "")[k+4] == "seven" {
+							line = append(line, 7)
+						}
+					}
+				case "e":
+					if k+4 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3]+strings.Split(i, "")[k+4] == "eight" {
+							line = append(line, 8)
+						}
+					}
+				case "n":
+					if k+3 < len(strings.Split(i, "")) {
+						if j+strings.Split(i, "")[k+1]+strings.Split(i, "")[k+2]+strings.Split(i, "")[k+3] == "nine" {
+							line = append(line, 9)
+						}
+					}
 				}
-			case "f":
-			case "s":
-			case "e":
-			case "n":
 			}
 		}
+		result += line[0]*10 + line[len(line)-1]
 	}
-	fmt.Println(numbers[""])
 	return result
 }
