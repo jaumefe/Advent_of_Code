21fb06367497616a6adac3529d5180619afc98ea
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2023-12-06 00:51:16 +0100

    Part 2 finished

--- 2023/Day_05/05.go
+++ 2023/Day_05/05.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	pt1 := part1(string(input1))
 	fmt.Println("The Fisrt part result is:", pt1)
-	input2, err := inputFS.ReadFile("inputs/test.txt")
+	input2, err := inputFS.ReadFile("inputs/input.txt")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -99,6 +99,11 @@ type maps struct {
 	dest   int
 	source int
 	length int
+}
+
+type seed struct {
+	origin int
+	dest   int
 }
 
 func translateFromMapToMap(input []int, mapData []maps) []int {
@@ -204,24 +209,30 @@ func part2(input string) int {
 
 	}
 	seedsIndex := strArrToIntArr(strings.Split(strings.Split(seeds[0], ": ")[1], " "))
-	var newSeeds []int
-
+	var seedMap []seed
 	for i := 0; i < len(seedsIndex); i += 2 {
-		for j := seedsIndex[i]; j < seedsIndex[i]+seedsIndex[i+1]; j++ {
-			newSeeds = append(newSeeds, j)
+		var seed seed
+		seed.origin = seedsIndex[i]
+		seed.dest = seedsIndex[i+1] + seedsIndex[i]
+		seedMap = append(seedMap, seed)
+	}
+	seedSoilMap, soilFerMap, ferWatMap, watLightMap, lightTempMap, tempHumMap, humLocMap := fillMapsData(seedToSoil), fillMapsData(soilToFer), fillMapsData(ferToWat), fillMapsData(watToLight), fillMapsData(lightToTemp), fillMapsData(tempToHum), fillMapsData(humToLoc)
+	soil := translateFromMapToMapPt2(seedMap, seedSoilMap)
+	fer := translateFromMapToMapPt2(soil, soilFerMap)
+	wat := translateFromMapToMapPt2(fer, ferWatMap)
+	light := translateFromMapToMapPt2(wat, watLightMap)
+	temp := translateFromMapToMapPt2(light, lightTempMap)
+	hum := translateFromMapToMapPt2(temp, tempHumMap)
+	loc := translateFromMapToMapPt2(hum, humLocMap)
+	var mins int
+	for _, i := range loc {
+		if mins == 0 {
+			mins = i.origin
+		} else if i.origin < mins {
+			mins = i.origin
 		}
 	}
-	//fmt.Println(newSeeds)
-	seedSoilMap, soilFerMap, ferWatMap, watLightMap, lightTempMap, tempHumMap, humLocMap := fillMapsData(seedToSoil), fillMapsData(soilToFer), fillMapsData(ferToWat), fillMapsData(watToLight), fillMapsData(lightToTemp), fillMapsData(tempToHum), fillMapsData(humToLoc)
-	soil := translateFromMapToMap(newSeeds, seedSoilMap)
-	fer := translateFromMapToMap(soil, soilFerMap)
-	wat := translateFromMapToMap(fer, ferWatMap)
-	light := translateFromMapToMap(wat, watLightMap)
-	temp := translateFromMapToMap(light, lightTempMap)
-	hum := translateFromMapToMap(temp, tempHumMap)
-	loc := translateFromMapToMap(hum, humLocMap)
-	sort.Ints(loc)
-	result = loc[0]
+	result = mins
 	return result
 }
 
@@ -231,4 +242,55 @@ func strArrToIntArr(input []string) []int {
 		res[i], _ = strconv.Atoi(j)
 	}
 	return res
+}
+func translateFromMapToMapPt2(input []seed, mapData []maps) []seed {
+	var result, result_1 []seed
+	for _, i := range input {
+		var seed_temp1 seed
+		var seed_temp2 seed
+		var seed_temp []seed
+		//length := len(result)
+		for _, j := range mapData {
+			if i.origin >= j.source && i.origin < j.source+j.length {
+				if i.dest < j.source+j.length {
+					seed_temp1.origin = j.dest + (i.origin - j.source)
+					seed_temp1.dest = seed_temp1.origin + (i.dest - i.origin)
+					result = append(result, seed_temp1)
+					break
+				} else {
+					seed_temp1.origin = j.dest + (i.origin - j.source)
+					seed_temp1.dest = j.dest + j.length - 1
+					result = append(result, seed_temp1)
+					seed_temp2.origin = j.source + j.length
+					seed_temp2.dest = i.dest
+					//result = append(result, seed_temp2)
+					seed_temp = append(seed_temp, seed_temp2)
+					result_1 = translateFromMapToMapPt2(seed_temp, mapData)
+					for _, k := range result_1 {
+						result = append(result, k)
+					}
+					break
+				}
+			}
+			if i.dest >= j.source && i.dest < j.source+j.length {
+				if i.origin <= j.source {
+					seed_temp1.origin = i.origin
+					seed_temp1.dest = j.source - 1
+					seed_temp2.origin = j.dest
+					seed_temp2.dest = j.dest + (i.dest - i.origin) - (seed_temp1.dest - seed_temp1.origin) - 1
+					result = append(result, seed_temp2)
+					seed_temp = append(seed_temp, seed_temp1)
+					result_1 = translateFromMapToMapPt2(seed_temp, mapData)
+					for _, k := range result_1 {
+						result = append(result, k)
+					}
+					break
+				}
+			}
+		}
+		if seed_temp1.origin == 0 && seed_temp1.dest == 0 {
+			result = append(result, i)
+		}
+	}
+	return result
 }
